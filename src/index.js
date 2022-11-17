ee8c700294c416f886396a34f3f93d45174c19f0
Richard Washington <richard.washington@taxassist.co.uk>
2022-11-17 23:18:11 +0000

    :sparkles: Add a project model class to create new project models

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './reset.css';
 import './styles.css';
+import projectsModel from './project-model';
 
 console.log('I am a to do app!');
+
+const project1 = new projectsModel('Project 1');
+project1.addTodo(1);
+
+console.log(project1.name + ': ' + project1.todos);
---
+++ src/project-model.js
@@ -0,0 +1,23 @@
+export default class Project {
+    constructor(name) {
+        this._todos = [];
+        this.name = name;
+    }
+
+    addTodo(todo) {
+        this._todos.push(todo);
+    }
+
+    get todos() {
+        return this._todos;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    
+    set name(name) {
+        this._name = name;
+    }
+}
