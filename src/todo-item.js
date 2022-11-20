f49f20e52b49ca7179026e67aaf22e9bee854c91
Richard Washington <richard.washington@taxassist.co.uk>
2022-11-20 17:22:11 +0000

    :sparkles: Add module for todo items

---
+++ src/todo-item.js
@@ -0,0 +1,30 @@
+export default class todo {
+    constructor(title, status) {
+        this._title = title;
+        this._status = status;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(title) {
+        this._title = title;
+    }
+
+    toggleStatus() {
+        if(this._status === true) {
+            this._status = false;
+        } else {
+            this._status = true;
+        }
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    set status(status) {
+        this._status = status;
+    }
+}
