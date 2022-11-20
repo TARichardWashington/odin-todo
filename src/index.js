46466b35bfc45e3444c76bf189a22fd585370e25
Richard Washington <richard.washington@taxassist.co.uk>
2022-11-20 19:13:31 +0000

    :lipstick: Add initial grid

--- src/index.js
+++ src/index.js
@@ -22,3 +22,65 @@ project1.addTodo(todo1);
 
 console.log(project1.todos);
 
+const doc = document.body;
+
+const nav = document.createElement('nav');
+nav.innerText = 'Todo';
+
+doc.appendChild(nav);
+
+const left = document.createElement('section');
+left.setAttribute('class', 'left');
+left.innerText = 'Menu';
+
+const listOfProjects = document.createElement('ul'); 
+
+const projects = [];
+
+projects.push(project1);
+
+projects.forEach(project => {
+    const projectItem = document.createElement('li');
+    projectItem.innerText = project.name;
+    listOfProjects.appendChild(projectItem);
+});
+
+left.appendChild(listOfProjects);
+
+doc.appendChild(left);
+
+const createProjectText = document.createElement('input');
+createProjectText.setAttribute('type', 'text');
+
+const createProjectButton = document.createElement('button');
+createProjectButton.innerText = 'Create project';
+
+createProjectButton.addEventListener('click', function() {
+    const project = new projectsModel(createProjectText.value);
+    createProjectText.value = '';
+    projects.push(project);
+
+    listOfProjects
+    .innerText = '';
+
+    projects.forEach(project => {
+        const projectItem = document.createElement('li');
+        projectItem.innerText = project.name;
+        listOfProjects.appendChild(projectItem);
+    });
+    
+    left.appendChild(listOfProjects);
+
+});
+
+left.appendChild(createProjectText);
+left.appendChild(createProjectButton);
+
+const right = document.createElement('section');
+right.setAttribute('class', 'right');
+
+doc.appendChild(right);
+
+
+
+
